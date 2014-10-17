db5675f7509450e8c04efe66b533778751151bca
Evan Simoni <evan.simoni@gmail.com>
2014-10-17 14:15:50 -0700

    adds meeting photo above contact page, removes footer

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-$(".flexnav").flexNav();
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+$(document).ready(function() {
+  $('a[rel="relativeanchor"').click(function(){
+      $('html, body').animate({
+          scrollTop: $( $.attr(this, 'href') ).offset().top
+      }, 500);
+      return false;
+  }); 
+});
